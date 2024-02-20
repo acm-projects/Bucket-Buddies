6a9f5d39e5dce59230cfa0c35919630ea2e15385
Erin Turgut <erintur23@gmail.com>
2024-02-19 20:52:34 -0600

    Added a welcome screen that links to a login screen and sign up screen

---
+++ lib/signup_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class  SignUpScreen extends StatefulWidget {
+
+  static const String id = 'signup_screen';
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
